d064e2e91d1b3fb83f4c2fdfd9ec01c479591c85
abazunts <bazunc@gmail.com>
2026-07-01 15:07:23 +0400

    update deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello DevOps v4.0';
+    return 'Hello DevOps v5.0';
   }
 }
